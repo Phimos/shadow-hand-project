92d4be6eca9aad7fa6d2b03433add6eeba6125a7
Yunchong Gan <yunchong@pku.edu.cn>
2023-02-01 14:37:25 +0800

    Create leap_motion_listener.py

---
+++ leap_motion_listener.py
@@ -0,0 +1,80 @@
+import sys
+
+import numpy as np
+
+import Leap
+
+
+def leap_vector_to_numpy(vector) -> np.ndarray:
+    """Converts a Leap Motion `Vector` to a numpy array.
+    """
+    return np.array([vector.x, vector.y, vector.z])
+    
+
+
+def leap_hand_to_keypoints(hand) -> np.ndarray:
+    """Converts a Leap Motion `Hand` to a numpy array of keypoints.
+    """
+    keypoints = np.zeros((21, 3))
+    
+    keypoints[0, :] = leap_vector_to_numpy(hand.palm_position)
+    
+    for finger in hand.fingers:
+        finger_index = finger.type
+        for bone_index in range(0, 4):
+            bone = finger.bone(bone_index)
+            index = 1 + finger_index * 4 + bone_index
+            keypoints[index, :] = leap_vector_to_numpy(bone.next_joint)
+                
+    return keypoints
+
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+    def on_init(self, controller):
+        print("Initialized")
+
+    def on_connect(self, controller):
+        print("Connected")
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print("Disconnected")
+
+    def on_exit(self, controller):
+        print("Exited")
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+        
+        print("Frame id %d" % frame.id)
+        for hand in frame.hands:
+            print("Left hand" if hand.is_left else "Right hand")
+            keypoints = leap_hand_to_keypoints(hand)
+            print(keypoints)
+
+def main():
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print("Press Enter to quit...")
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
