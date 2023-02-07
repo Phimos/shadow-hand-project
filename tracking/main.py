dc3e8b4ce5091e6e9e0b7c1faf59858b84e80d84
Phimos <yunchong@pku.edu.cn>
2023-02-07 13:13:24 +0800

    <fix> normalized the error

--- tracking/main.py
+++ tracking/main.py
@@ -30,6 +30,11 @@ def error_ellipse_fitting(
     a, b = a / 2, b / 2
     theta = np.radians(angle)
 
+    scale = 1 / np.sqrt(a**2 + b**2)
+    cx, cy = cx * scale, cy * scale
+    a, b = a * scale, b * scale
+    points = points * scale
+
     A = (a * np.sin(theta)) ** 2 + (b * np.cos(theta)) ** 2
     B = 2 * (b**2 - a**2) * np.sin(theta) * np.cos(theta)
     C = (a * np.cos(theta)) ** 2 + (b * np.sin(theta)) ** 2
@@ -39,8 +44,9 @@ def error_ellipse_fitting(
 
     x, y = points[:, 0], points[:, 1]
 
+    # Equation: Ax^2 + Bxy + Cy^2 + Dx + Ey + F = 0
     error = (A * x**2 + B * x * y + C * y**2 + D * x + E * y + F) ** 2
-    return error.sum() / (F**2)
+    return error.mean() / (F**2)
 
 
 if __name__ == "__main__":
@@ -88,6 +94,7 @@ if __name__ == "__main__":
     cv2.imshow("Contours", find_image)
     cv2.waitKey(0)
 
+    # circle_image = original.copy()
     circle_image = find_image.copy()
     contours, hierarchy = cv2.findContours(
         find_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
@@ -97,9 +104,12 @@ if __name__ == "__main__":
         center, size, angle = cv2.fitEllipse(contours[i])
         error = error_ellipse_fitting((center, size, angle), contours[i])
         center = tuple(map(int, center))
-        if error > 1e-6:
+        if error > 1e-8 or max(size) / min(size) > 2:
             continue
-        circle_image = cv2.drawMarker(circle_image, center, 255, cv2.MARKER_CROSS)
+        print("Index", i, "Error", error, "Size", size, "Angle", angle)
+        circle_image = cv2.drawMarker(
+            circle_image, center, (0, 0, 255), cv2.MARKER_CROSS
+        )
         cv2.putText(circle_image, str(i), center, cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
         circle_image = cv2.ellipse(circle_image, (center, size, angle), 255, 2)
     cv2.imshow("Circle", circle_image)
